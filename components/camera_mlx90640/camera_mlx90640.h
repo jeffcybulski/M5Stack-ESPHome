8741e3cfa8238eb06c56e290099fdc4d5babf775
Jeff Cybulski <jeff.cybulski@gmail.com>
2025-01-03 19:14:40 +0100

    Update camera_mlx90640.h

--- components/camera_mlx90640/camera_mlx90640.h
+++ components/camera_mlx90640/camera_mlx90640.h
@@ -9,6 +9,8 @@
 #include "MLX90640_I2C_Driver.h"
 #include <Wire.h>
 
+// ADDED CODE FOR i2c_id SUPPORT:
+#include "esphome/components/i2c/i2c.h"  // <-- ADDED CODE: So we can reference i2c::I2CBus
 
 namespace esphome {
     namespace mlx90640_app{
@@ -33,6 +35,10 @@ namespace esphome {
                 sensor::Sensor *median_temperature_sensor_{nullptr};
                 //sensor::Sensor *min_index ;
                 // sensor::Sensor *max_index ;
+
+                // ADDED CODE FOR i2c_id:
+                i2c::I2CBus *i2c_bus_{nullptr}; // <-- ADDED CODE: We'll store a pointer to the i2c bus if set
+                
               public:
                 MLX90640(web_server_base::WebServerBase *base);
                float get_setup_priority() const override { return setup_priority::LATE; }
@@ -55,6 +61,10 @@ namespace esphome {
                // filtering function
                void set_filter_level(float level){this->filter_level_ = level ;}
                void filter_outlier_pixel(float *pixels , int size , float level);
+               
+               // ADDED CODE FOR i2c_id:
+               // This method will be called from Python if the user sets i2c_id in YAML
+               void set_i2c_bus(i2c::I2CBus *bus) { this->i2c_bus_ = bus; }
                
         };
     }
